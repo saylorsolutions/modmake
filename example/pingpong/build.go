10e6359af5038a5cc5bc892495d55233bb5d086b
Doug Saylor <doug@saylorsolutions.com>
2023-09-17 05:06:03 -0500

    Simplifying pingpong build

--- example/pingpong/build.go
+++ example/pingpong/build.go
@@ -2,15 +2,37 @@ package main
 
 import (
 	. "github.com/saylorsolutions/modmake"
-	client "github.com/saylorsolutions/modmake/example/pingpong/client/build"
-	server "github.com/saylorsolutions/modmake/example/pingpong/server/build"
 )
 
 func main() {
 	b := NewBuild()
-	b.Import("client", client.Import())
-	b.Import("server", server.Import())
+	b.Import("client", client())
+	b.Import("server", server())
 	b.Build().DependsOn(b.Step("client:build"))
 	b.Build().DependsOn(b.Step("server:build"))
 	b.Execute()
+}
+
+func client() *Build {
+	mainPath := Go().ToModulePath("example/pingpong/client/main.go")
+	b := NewBuild()
+	b.Build().Does(
+		Go().Build(mainPath).OutputFilename("client_test"),
+	)
+	b.AddStep(NewStep("run", "Runs the client").Does(
+		Go().Run(mainPath)),
+	)
+	return b
+}
+
+func server() *Build {
+	mainPath := Go().ToModulePath("./example/pingpong/server/main.go")
+	b := NewBuild()
+	b.Build().Does(
+		Go().Build(mainPath).OutputFilename("server_test"),
+	)
+	b.AddStep(NewStep("run", "Runs the server").Does(
+		Go().Run(mainPath)),
+	)
+	return b
 }
