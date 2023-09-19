8d3f315114a27ba3bba7aeb2b3a211706dd2f2b3
Doug Saylor <doug@saylorsolutions.com>
2023-09-18 23:20:19 -0500

    Adding basic download functionality
     - May need to add a standalone type to account for things like response code or request method differences, and redirect logic.

---
+++ http.go
@@ -0,0 +1,49 @@
+package modmake
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func Download(url, location string) Task {
+	url = strings.TrimSpace(url)
+	location = strings.TrimSpace(location)
+	if len(url) == 0 {
+		panic("empty URL")
+	}
+	if len(location) == 0 {
+		panic("empty download location")
+	}
+	return func(ctx context.Context) error {
+		req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+		if err != nil {
+			return fmt.Errorf("failed to create request: %v", err)
+		}
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return fmt.Errorf("failed to send HTTP request: %v", err)
+		}
+		defer func() {
+			if resp.Body != nil {
+				_ = resp.Body.Close()
+			}
+		}()
+		if resp.StatusCode != 200 {
+			return fmt.Errorf("expected status 200 OK, got %s", resp.Status)
+		}
+
+		out, err := os.Create(location)
+		if err != nil {
+			return err
+		}
+		defer func() {
+			_ = out.Close()
+		}()
+		_, err = io.Copy(out, resp.Body)
+		return err
+	}
+}
